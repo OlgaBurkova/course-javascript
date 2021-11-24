031a505b333a8a72b5c8944c38a0b28e17bcb1b2
Olga Burkova <cherry-olechka1993@yandex.ru>
2021-11-24 22:43:36 +0300

    6 homework

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,70 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+let dragObject = {};
 
-export function createDiv() {}
+function onMouseDown(e) {
+  if (e.which !== 1) return;
+
+  const elem = e.target.closest('.draggable-div');
+  if (!elem) return;
+
+  dragObject.elem = elem;
+
+  dragObject.downX = e.pageX;
+  dragObject.downY = e.pageY;
+
+  const coords = getCoords(dragObject.elem);
+  dragObject.shiftX = dragObject.downX - coords.left;
+  dragObject.shiftY = dragObject.downY - coords.top;
+
+  elem.style.position = 'absolute';
+  return false;
+}
+
+document.addEventListener('mousemove', (e) => {
+  if (!dragObject.elem) {
+    return;
+  }
+
+  dragObject.elem.style.left = e.pageX - dragObject.shiftX + 'px';
+  dragObject.elem.style.top = e.pageY - dragObject.shiftY + 'px';
+
+  return false;
+});
+
+function onMouseUp(e) {
+  dragObject = {};
+}
+
+document.onmouseup = onMouseUp;
+document.onmousedown = onMouseDown;
+
+function getCoords(elem) {
+  const box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset,
+  };
+}
+
+function createDiv() {
+  const element = document.createElement('div');
+  element.classList.add('draggable-div');
+  element.draggable = true;
+  element.style.width = Math.random() * 300 + 'px';
+  element.style.height = Math.random() * 300 + 'px';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += parseInt(Math.random() * 16).toString(16);
+  }
+  element.style.backgroundColor = color;
+  element.style.top = Math.random() * 300 + 'px';
+  element.style.left = Math.random() * 300 + 'px';
+
+  return element;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
@@ -29,3 +90,5 @@ addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
 });
+
+export { createDiv };
